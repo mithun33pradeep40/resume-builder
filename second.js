6474e5e74059a0e96f257acb84902ae5e1b31d05
mithun33pradeep40 <mithun33pradeep40@gmail.com>
2025-02-26 11:52:46 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,10 @@
+
+document.getElementById("template 1").addEventListener("click", function () {
+    window.location.href ="third.html";
+});
+document.getElementById("template 2").addEventListener("click", function () {
+    window.location.href ="third.html";
+});
+document.getElementById("template 3").addEventListener("click", function () {
+    window.location.href ="third.html";
+});
